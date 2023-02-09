544abd7564839ecc52df2be0b06c755de486421e
BRO <ayoolacoul27@gmail.com>
2023-02-09 11:43:44 -0600

    added some extra outputs

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,31 @@ output "execution_arn" {
   description = "The Execution ARN of the REST API."
 }
 
+output "lambda_without_vpc_invoke_arn" {
+  value       = aws_lambda_function.default_without_vpc[0].invoke_arn
+  description = "The Execution ARN of the REST API."
+}
+
+output "lambda_with_vpc_invoke_arn" {
+  value       = aws_lambda_function.default_with_vpc[0].invoke_arn
+  description = "The Execution ARN of the REST API."
+}
+
+output "authorizer_without_vpc_invoke_arn" {
+  value       = aws_lambda_function.authorizer_without_vpc[0].invoke_arn
+  description = "The Execution ARN of the REST API."
+}
+
+output "authorizer_with_vpc_invoke_arn" {
+  value       = aws_lambda_function.authorizer_with_vpc[0].invoke_arn
+  description = "The Execution ARN of the REST API."
+}
+
+
 # output "tags" {
 #   value       = module.labels.tags
 #   description = "A mapping of tags to assign to the resource."
 # }
+
+
+
