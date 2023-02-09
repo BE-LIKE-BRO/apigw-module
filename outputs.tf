841303dbd5f1c1a4a8486f213e5b3bc2092d386d
BRO <ayoolacoul27@gmail.com>
2023-02-09 13:00:01 -0600

    modified output

--- outputs.tf
+++ outputs.tf
@@ -8,26 +8,16 @@ output "execution_arn" {
   description = "The Execution ARN of the REST API."
 }
 
-output "lambda_without_vpc_invoke_arn" {
-  value       = aws_lambda_function.default_without_vpc[0].invoke_arn
+output "lambda_default_invoke_arn" {
+  value       = var.attach_vpc_config ? aws_lambda_function.default_with_vpc[0].invoke_arn : aws_lambda_function.default_without_vpc[0].invoke_arn
   description = "The Execution ARN of the REST API."
 }
 
-output "lambda_with_vpc_invoke_arn" {
-  value       = aws_lambda_function.default_with_vpc[0].invoke_arn
+
+output "authorizer_invoke_arn" {
+  value       = var.attach_vpc_config ? aws_lambda_function.authorizer_with_vpc[0].invoke_arn : aws_lambda_function.authorizer_without_vpc[0].invoke_arn
   description = "The Execution ARN of the REST API."
 }
-
-output "authorizer_without_vpc_invoke_arn" {
-  value       = aws_lambda_function.authorizer_without_vpc[0].invoke_arn
-  description = "The Execution ARN of the REST API."
-}
-
-output "authorizer_with_vpc_invoke_arn" {
-  value       = aws_lambda_function.authorizer_with_vpc[0].invoke_arn
-  description = "The Execution ARN of the REST API."
-}
-
 
 # output "tags" {
 #   value       = module.labels.tags
